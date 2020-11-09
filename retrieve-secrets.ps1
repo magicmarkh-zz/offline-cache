9cedb43437545615b20a960fecccf87d08e39fce
Mark Hurter <33732916+magicmarkh@users.noreply.github.com>
2020-11-09 14:44:49 -0600

    updated with hardening changes

--- retrieve-secrets.ps1
+++ retrieve-secrets.ps1
@@ -1,14 +1,17 @@
-#set your PVWA address
-$pvwa_address = "1.1.1.1"
+#variables
+$pvwa_address = "" #your PVWA VIP/IP Address. do not include https, ex: your.company.com
+$csv_full_path = "" #full path to CSV file create in sync-secrets.ps1
+$app_id = "" #your app id
+$secrets_safe_name = "" #name of safe secrets will be retrieved from
 
-$csv_full_path = "C:\tmp\objectids.csv"
 #run a test to see if the user can go to the web portal before returning credentials
 $pvwa_status = Test-NetConnection $pvwa_address -Port 443
 
 #if users can access PVWA, do not allow them to retrieve credentials via script, exit early
 if ($true -eq $pvwa_status.TcpTestSucceeded)
 {
-    Write-Host "PVWA is up, use web portal for privileged credentials access. Exiting."
+    Write-Host "PVWA is up, use web portal for privileged credentials access. Press any key to continue."
+    Pause
     exit
 }
 
@@ -17,8 +20,8 @@ $credentials_array = @()
 
 #for each item retrieved, create a row with password properties and add to the array
 Import-Csv $csv_full_path | ForEach-Object{
-    $results = & 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'GetPassword /p AppDescs.AppID=offline1 /p Query="Safe=ship1;Folder=Root;Object=$($_.name)" /p RequiredProps=UserName,Address /o PassProps.UserName,PassProps.Address,Password
-    $credential = $results.Split(",")
+    $results = cmd /c 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'GetPassword /p AppDescs.AppID=$app_id /p Query="Safe=$($secrets_safe_name);Object=$($_.name)" /p RequiredProps=UserName,Address /o PassProps.UserName,PassProps.Address,Password
+    $credential = $results.split(",")
     $row = New-Object psobject
     $row | Add-Member -NotePropertyName Username -NotePropertyValue $credential[0]
     $row | Add-Member -NotePropertyName Address -NotePropertyValue $credential[1]
@@ -27,5 +30,5 @@ Import-Csv $csv_full_path | ForEach-Object{
 }
 
 #write the credentials to the console
-$credentials_array
-
+$credentials_array | ForEach-Object { [PSCustomObject] $_ } | Format-Table
+Pause
--- sync-secrets.ps1
+++ sync-secrets.ps1
@@ -1,8 +1,13 @@
 #set your variables
-$pvwa_uri = "https://pvwa.yourcompany.com"
-$csv_folder_path = "C:\tmp"
-$csv_file_name = "objectids.csv"
+$pvwa_uri = "" #address of your pvwa. ex: https://your.company.com
+$csv_folder_path = "" #path of folder where the CSV will be stored
+$csv_file_name = "" #name of file ex: objectids.csv
 $csv_full_path = $csv_folder_path + "\" + $csv_file_name
+$app_id = "" #appid must be configured in CyberArk
+$api_user_safe_name = "" #safe where user that will connect to API is stored
+$api_user_object_name = "" #object name of API user
+$api_user_username = "" #username of API user
+$sync_safe_name = "" #name of safe to sync
 
 #test to see if your csv exists. If not, create it
 if (!(Test-Path $csv_full_path))
@@ -14,23 +19,20 @@ if (!(Test-Path $csv_full_path))
 $csv = Import-Csv -Path $csv_full_path
 
 #get API user password
-$api_password = & 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'GetPassword /p AppDescs.AppID=offline1 /p Query="Safe=ship1;Folder=Root;Object=ship1_sync" /o Password | ConvertTo-SecureString -AsPlainText -Force
-$api_credential = New-Object System.Management.Automation.PSCredential ("ship1_sync", $api_password)
-
+$api_password = cmd /c 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'GetPassword /p AppDescs.AppID=$app_id /p "Query=Safe=$api_user_safe_name;Object=$api_user_object_name" /o password | ConvertTo-SecureString -AsPlainText -Force
+$api_credential = New-Object System.Management.Automation.PSCredential ($api_user_username, $api_password)
 #start new session
-New-PASSession -Credential $api_credential -BaseURI $pvwa_uri
+New-PASSession -Credential $api_credential -BaseURI $pvwa_uri -type LDAP
 
 #get all accounts in the first safe
-$accounts = Get-PASAccount -filter "SafeName eq ship1"
+$accounts = Get-PASAccount -filter "SafeName eq $($sync_safe_name)"
 
 #loop through all accounts in the safe. If it's already in the csv, nothing to do, if not, retrieve the cred for storage
 # in the Credential Provider & add the object name to the list of objects in the vault to retrieve during an outage 
 foreach($account in $accounts){
     if($account.name -notin $csv.name)
     {
-        & 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'GetPassword /p AppDescs.AppID=offline1 /p Query="Safe=ship1;Folder=Root;Object=$($account.name)"
-        $account | Select-Object -Property name, username | Export-Csv -Path $csv_full_path -Append
+        cmd /c 'C:\Program Files (x86)\CyberArk\ApplicationPasswordSdk\CLIPasswordSDK.exe'GetPassword /p AppDescs.AppID=$app_id /p "Query=Safe=$sync_safe_name;Object=$($account.name)"
+        $account | Select-Object -Property name | Export-Csv -Path $csv_full_path -Append
     }
 }
-
-Close-PASSession
